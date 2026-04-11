1808050330668dfea415ffbaa5c39ec05817e601
r.mavericks <r.mavericks@gmail.com>
2026-04-11 20:32:39 +0800

    update mainjs and Appvue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div>
+    <NavBar v-if="isLoggedIn" />
+    <div class="d-flex">
+      <SideBar v-if="isLoggedIn" />
+      <main class="flex-grow-1 p-4">
+        <RouterView />
+      </main>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { computed } from 'vue'
+import { useAuthStore } from '@/stores/authStore'
+import NavBar from '@/components/NavBar.vue'
+import SideBar from '@/components/SideBar.vue'
+
+const authStore  = useAuthStore()
+const isLoggedIn = computed(() => authStore.isLoggedIn)
+</script>
